f7b3f4feae896d6fb3f538744a5b4133e22141ce
manhavn <manhavn@outlook.com>
2023-01-02 10:08:12 +0700

    add plugin chat

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,31 @@ export default {
     onMounted(() => {
       logging("mounted");
       loggingTable(["mounted"]);
+
+      (function (d, s, id) {
+        let js,
+          fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+        fjs.parentNode.insertBefore(js, fjs);
+      })(document, "script", "facebook-jssdk-autoketing");
+
+      const chatbox = document.getElementById("fb-customer-chat");
+      chatbox.setAttribute("page_id", ".....");
+      chatbox.setAttribute("attribution", "biz_inbox");
+
+      window.fbAsyncInit = function () {
+        window.FB.init({
+          appId: ".....",
+          autoLogAppEvents: false,
+          cookie: true,
+          status: true,
+          xfbml: true,
+          version: "v15.0",
+        });
+      };
     });
     return {};
   },
@@ -23,5 +48,8 @@ export default {
   <main>
     <img alt="Vue logo" class="logo" src="./assets/logo.png" />
     <HelloWorld msg="Hello World!" />
+    <!-- Messenger Chat Plugin Code -->
+    <div id="fb-root"></div>
+    <div id="fb-customer-chat" class="fb-customerchat"></div>
   </main>
 </template>
